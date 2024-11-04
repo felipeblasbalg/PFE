9da3628bde5f4656a3d8579d5190e844d97b1fb9
Enzo Cunha <81170064+enzosvc@users.noreply.github.com>
2024-11-04 16:28:35 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Configuração da página
-st.set_page_config(page_title="Análise de Dados", layout="wide")
+#st.set_page_config(page_title="Análise de Dados", layout="wide")
 image1 = "logoInsper3.png"
 image2 = "logoAlupar.png"
 
