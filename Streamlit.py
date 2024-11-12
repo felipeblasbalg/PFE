56a2797ff2be6d03daf8661c32e27ddb58183e96
Enzo Cunha <81170064+enzosvc@users.noreply.github.com>
2024-11-12 00:45:58 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 from analysis import Analysis
-
+import plotly.graph_objects as go
+lista_prediction_anteriores = []
+contador = 1
+num_ciclos = []
 # Configuração da página e inicialização do objeto de análise
 st.set_page_config(page_title="Análise de Dados", layout="wide")
 image1 = "logoInsper3.png"
@@ -97,7 +100,14 @@ def upload_page():
                         st.session_state["previsoes_ultimos_ciclos"] = prediction[0]
                         st.session_state["proxima_falha_ciclos"] = prediction[0][-1]
                         st.session_state["proxima_falha_segundos"] = prediction[1] * prediction[0][-1]
-
+                           
+                        lista_prediction_anteriores = []
+                        contador = 0
+                        while len(lista_prediction_anteriores) >= 29:
+                            contador += 1
+                            lista_prediction_anteriores.append(prediction[0][-(contador)])
+                        lista_prediction_anteriores = lista_prediction_anteriores[::-1]
+                                               
                         print(st.session_state["previsoes_ultimos_ciclos"])
 
                         st.session_state['data_verificada'] = True
@@ -176,7 +186,22 @@ def results_page():
     # Adicionando um divisor visual
     st.markdown("<div style='height: 2px; background-color: #007bff; margin: 20px 0;'></div>", unsafe_allow_html=True)
     
-
+    # Criação do gráfico interativo
+    fig = go.Figure()
+    
+    # Adicionando a linha ao gráfico
+    fig.add_trace(go.Scatter(x=num_ciclos, y=lista_prediction_anteriores, mode='lines+markers', name='Previsão de Falha'))
+    
+    # Títulos e rótulos dos eixos
+    fig.update_layout(
+        title='Previsão de Falha ao Longo dos Ciclos',
+        xaxis_title='Número do Ciclo',
+        yaxis_title='Previsão de Falha (Ciclos)',
+        template='plotly_dark',  # Usando um tema escuro para o gráfico
+    )
+    
+    # Exibindo o gráfico no Streamlit
+    st.plotly_chart(fig)
 
     if st.button("Voltar à Página Principal"):
         st.session_state['current_page'] = 'upload_page'
